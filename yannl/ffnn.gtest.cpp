b448fc7a765d55451ea3fa8ba176e500ae8e8901
David Bennett <david.bennett@sias.com>
2017-03-09 14:02:20 +0000

    Basic implementation of a feed forward network

--- yannl/ffnn.gtest.cpp
+++ yannl/ffnn.gtest.cpp
@@ -6,7 +6,7 @@ using namespace yannl;
 
 float Tolerance = 0.00001f;
 
-TEST(ffnn, evaluate)
+TEST(ffnn, evaluate_no_hidden_layers)
 {
     FFNN ffnn(1, 1, 0, 0);
 
@@ -21,4 +21,42 @@ TEST(ffnn, evaluate)
     std::vector<float> output = evaluate({ 1.2f }, ffnn);
 
     EXPECT_NEAR(output[0], 0.61301f, Tolerance);
+}
+
+TEST(ffnn, evaluate_invalid_network)
+{
+    EXPECT_DEATH(FFNN(0, 0, 0, 0), "");
+    EXPECT_DEATH(FFNN(1, 0, 0, 0), "");
+    EXPECT_DEATH(FFNN(0, 1, 0, 0), "");
+    EXPECT_DEATH(FFNN(-1, 1, 0, 0), "");
+    EXPECT_DEATH(FFNN(1, -1, 0, 0), "");
+    EXPECT_DEATH(FFNN(1, 1, -1, 0), "");
+    EXPECT_DEATH(FFNN(1, 1, 0, -1), "");
+    EXPECT_DEATH(FFNN(1, 1, 1, 0), ""); // Layer with no nodes
+
+    // Too few inputs
+    FFNN network(1, 1, 0, 0);
+
+    EXPECT_DEATH(evaluate({}, network), "");
+    EXPECT_DEATH(evaluate({ 1, 2 }, network), "");
+}
+
+TEST(ffnn, evaluate_with_hidden_layers)
+{
+    FFNN ffnn(2, 4, 2, 3);
+
+    EXPECT_EQ(ffnn.numberOfInputs(), 2);
+    EXPECT_EQ(ffnn.numberOfOutputs(), 4);
+    EXPECT_EQ(ffnn.numberOfHiddenLayers(), 2);
+    EXPECT_EQ(ffnn.numberOfNodesPerHiddenLayer(), 3);
+    EXPECT_EQ(ffnn.numberOfWeights(), 37);
+
+    ffnn.getWeights() = { -0.996777454f, -0.863441195f, 0.470315692f, -0.377533666f, 0.239034347f, -0.700948593f, 0.727923212f, 0.032970345f, 0.897148472f, 0.155163534f, -0.348505041f, -0.690309079f, -0.342748347f, 0.489008183f, 0.401770705f, 0.774834195f, 0.550771839f, 0.213473774f, -0.540993331f, -0.170803675f, 0.277790251f, -0.556754311f, -0.76309386f, 0.378873368f, 0.49559025f, -0.643425222f, 0.507739436f, -0.258631964f, -0.26149227f, -0.651029465f, 0.545696603f, -0.691562398f, 0.700588407f, 0.755926461f, 0.80508147f, -0.642217184f, 0.676940276f };
+
+    std::vector<float> output = evaluate({ 1.2f, 1.3f }, ffnn);
+
+    EXPECT_NEAR(output[0], 0.26447f, Tolerance);
+    EXPECT_NEAR(output[1], 0.53950f, Tolerance);
+    EXPECT_NEAR(output[2], 0.28271f, Tolerance);
+    EXPECT_NEAR(output[3], 0.46847f, Tolerance);
 }
--- yannl/ffnn.hpp
+++ yannl/ffnn.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cassert>
 #include <vector>
 
 namespace yannl
@@ -14,14 +15,21 @@ namespace yannl
             , mNumberOfHiddenLayers(aNumberOfHiddenLayers)
             , mNumberOfNodesPerHiddenLayer(aNumberOfNodesPerHiddenLayer)
         {
-            mWeights.resize(numberOfWeights(), 0);
+            assert(aNumberOfInputs > 0);
+            assert(aNumberOfOutputs > 0);
+            assert(aNumberOfHiddenLayers >= 0);
+            assert(aNumberOfNodesPerHiddenLayer >= 0);
+            assert(aNumberOfHiddenLayers == 0 || aNumberOfNodesPerHiddenLayer > 0);
+
+            int weightCount = mNumberOfHiddenLayers > 0 ? mNumberOfOutputs * (mNumberOfNodesPerHiddenLayer + 1) + mNumberOfNodesPerHiddenLayer * (mNumberOfInputs + 1) + (mNumberOfHiddenLayers - 1) * mNumberOfNodesPerHiddenLayer * (mNumberOfNodesPerHiddenLayer + 1) : mNumberOfOutputs * (mNumberOfInputs + 1);
+            mWeights.resize(weightCount, 0);
         }
 
         int numberOfInputs() const { return mNumberOfInputs; }
         int numberOfOutputs() const { return mNumberOfOutputs; }
         int numberOfHiddenLayers() const { return mNumberOfHiddenLayers; }
         int numberOfNodesPerHiddenLayer() const { return mNumberOfNodesPerHiddenLayer; }
-        int numberOfWeights() { return (mNumberOfOutputs * (mNumberOfInputs + 1)); }
+        int numberOfWeights() { return mWeights.size(); }
 
         std::vector<float> & getWeights() { return mWeights; }
 
@@ -36,16 +44,34 @@ namespace yannl
 
     std::vector<float> evaluate(const std::vector<float> & aInputs, FFNN & aFFNN)
     {
+        assert(aInputs.size() == aFFNN.numberOfInputs());
+
         const std::vector<float> & weights = aFFNN.getWeights();
         float bias = -1;
 
-        float output = 0;
-        for (float input : aInputs)
-        {
-            output += input * weights[0];
-        }
-        output += bias * weights[1];
+        int weightIndex = 0;
+        std::vector<float> inputs = aInputs;
+        std::vector<float> outputs;
 
-        return { (1 / (1 + exp(-output))) };
+        for (int layer = 0; layer < aFFNN.numberOfHiddenLayers() + 1; ++layer)
+        {
+            outputs.clear();
+            outputs.resize(layer == aFFNN.numberOfHiddenLayers() ? aFFNN.numberOfOutputs() : aFFNN.numberOfNodesPerHiddenLayer(), 0);
+            for (int node = 0; node < (int)outputs.size(); ++node)
+            {
+                float & sum = outputs[node];
+
+                for (int inputIndex = 0; inputIndex < (int)inputs.size(); ++inputIndex)
+                {
+                    sum += weights[weightIndex++] * inputs[inputIndex];
+                }
+                sum += weights[weightIndex++] * bias;
+                sum = (1 / (1 + exp(-sum)));
+            }
+
+            inputs = outputs;
+        }
+
+        return outputs;
     }
 }
